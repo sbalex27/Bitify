5b5069054b868bb70494f5ac56cb88e1cf373393
Alex <alex2000.sabe@gmail.com>
2024-04-13 11:46:33 -0600

    Add project files.

---
+++ Bitify/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitify
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to convert to binary:");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Binary: " + Convert.ToString(number, 2));
+            Console.ReadLine();
+        }
+    }
+}
